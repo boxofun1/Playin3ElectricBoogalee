42cf6ff01481a4de71f51f2a4f0b33eec9fb9df3
David Galanter <galanter_d@yahoo.com>
2021-03-02 17:04:06 -0800

    Random tests with scripting in Unity

---
+++ Assets/Scripts/Bike.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike
+{
+	private string name;
+	private float speed;
+	private int nbWheels;
+
+	public Bike()
+	{
+		name = "Just another bike";
+		speed = 0.0f;
+	}
+
+	public Bike(string newName)
+	{
+		name = newName;
+		speed = 0.0f;
+		Debug.Log("Just created a new bike with the name " + name);
+	}
+
+	public void accelerate()
+	{
+		speed += 1;
+		Debug.Log("Our new speed is now " + speed);
+	}
+}
---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    private int number;
+    private string myName;
+    private int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //number = 1;
+        //myName = "David";
+        ////print("Hello " + myName + ". Your number is " + number);
+
+        //int localVariable = 3;
+        ////print("local variable: " + localVariable);
+        //counter = 0;
+
+        //myFirstMethodApparently();
+
+        Bike b1 = new Bike("My First Bike");
+        b1.accelerate();
+        b1.accelerate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(myName);
+        //print("local variable: " + localVariable);
+        counter++;
+        //print("counter =" + counter);
+    }
+
+    private void myFirstMethodApparently()
+	{
+        print("Sup Planet");
+	}
+}
